f75bf369b7c3069f1ef664f9320d0f13e4c7e47f
Steve Mynott <steve.mynott@gmail.com>
2021-11-04 11:50:22 +0000

    checkpoint

--- aws-database/import.sh
+++ aws-database/import.sh
@@ -1,12 +1,30 @@
 #!/bin/bash -x
 
 DB="insights"
+USER="$DB"
+PW="$DB"
+#DATA="data"
+DATA="fi-census-data/2011/atlas/viv"
 
-cd data
+dropdb "$B"
+createdb "$B"
+
+if [ ! -d "pgfutter" ]; then
+    git clone -b develop git@github.com:stmuk/pgfutter.git
+    cd pgfutter && go build
+    cd ..
+fi
+
+if [ ! -d "$DATA" ]; then
+    echo "data dir must exist"
+    exit 1
+fi
+
+cd "$DATA"
 for i in `ls *.csv`; do
     table=${i%\.csv}
     echo $table
-    ../pgfutter/pgfutter --host localhost -db "$DB" --table $table --schema atlas2011 -user "$DB" --pw "$DB" csv $i
+    ATLAS=1 ../pgfutter/pgfutter --host localhost -db "$DB" --table $table --schema atlas2011 -user "$USER" --pw "$PW" csv "$i"
 done
 
 cd ..
