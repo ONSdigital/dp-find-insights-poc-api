156436ba5b2799f16de7277690f527c97b90ba61
Steve Mynott <steve.mynott@gmail.com>
2021-11-11 11:46:20 +0000

    import "new db schema" & creatschema

---
+++ cmd/creatschema/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/ONSdigital/dp-find-insights-poc-api/model"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func main() {
+	dsn := fmt.Sprintf(
+		"postgres://%s:%s@%s:%s/%s",
+		os.Getenv("PGUSER"),
+		os.Getenv("PGPASSWORD"),
+		os.Getenv("PGHOST"),
+		os.Getenv("PGPORT"),
+		os.Getenv("PGDATABASE"),
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Print(err)
+	}
+
+	// XXX create/alter tables - doesn't delete cols or tables!
+	// neither does it always change types correctly
+	// More useful in dev than prod
+
+	if err := db.AutoMigrate(
+		&model.GeoType{},
+		&model.Geo{},
+		&model.NomisDesc{},
+		&model.NomisCategory{},
+		&model.GeoMetric{},
+	); err != nil {
+		log.Print(err)
+	}
+}
