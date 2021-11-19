07ca244a60c4d9dd7f148fd2fb69306b330e2e85
Daniel Lawrence <33934706+wavemechanics@users.noreply.github.com>
2021-11-19 06:31:58 +0000

    replace table pkg

--- pkg/table/table_test.go
+++ pkg/table/table_test.go
@@ -1,157 +1,93 @@
-package table
+package table_test
 
 import (
-	"fmt"
 	"strings"
 	"testing"
+
+	"github.com/ONSdigital/dp-find-insights-poc-api/pkg/table"
 )
 
-func TestNewErrors(t *testing.T) {
-	primary := "GEO"
-	var tests = []struct {
-		primary  string
-		colnames []string
-	}{
-		{"", []string{"COLA"}},
-		{primary, []string{}},
-		{primary, []string{""}},
-		{primary, []string{"COLA", "COLA"}},
-	}
-
-	for _, test := range tests {
-		tbl, err := New(test.primary, test.colnames)
-		if err == nil {
-			t.Errorf("expected error")
-			continue
-		}
-		if tbl != nil {
-			t.Errorf("expected tbl to be nil")
-		}
-	}
-}
-
-func TestNew(t *testing.T) {
-	primary := "GEO"
-
-	var tests = []struct {
-		colnames []string
-		want     string
-	}{
-		{[]string{"COL"}, fmt.Sprintf("%s,COL\n", primary)},
-		{[]string{"COLA", "COLB"}, fmt.Sprintf("%s,COLA,COLB\n", primary)},
-	}
-
-	for _, test := range tests {
-		tbl, err := New(primary, test.colnames)
-		if err != nil {
-			t.Error(err)
-			continue
-		}
-		buf := strings.Builder{}
-		err = tbl.Generate(&buf)
-		if err != nil {
-			t.Error(err)
-			continue
-		}
-		if buf.String() != test.want {
-			t.Errorf("got %q, want %q", buf.String(), test.want)
-			continue
-		}
-	}
-}
-
-func TestColIndex(t *testing.T) {
-	var tests = []struct {
-		colname string
-		want    int
-	}{
-		{"", -1},
-		{"noexist", -1},
-		{"COLA", 1},
-		{"COLB", 2},
-	}
-
-	tbl, err := New("GEO", []string{"COLA", "COLB"})
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	for _, test := range tests {
-		i := tbl.colIndex(test.colname)
-		if i != test.want {
-			t.Errorf("%s: %d, want %d", test.colname, i, test.want)
-		}
-	}
-}
-
-func TestFindRow(t *testing.T) {
-	tbl, err := New("GEO", []string{"COL"})
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	code := "GEO"
-	row := tbl.findRow(code)
-	if row[0] != code {
-		t.Errorf("expected new row[0] to be %s", code)
-	}
-
-	if len(tbl.rows) != 1 {
-		t.Errorf("expected table to have one row")
-	}
-
-	if tbl.rows[0][0] != code {
-		t.Errorf("expected table[0][0] to be %s", code)
-	}
-
-	row = tbl.findRow(code)
-	if row[0] != code {
-		t.Errorf("expected existing row[0] to be %s", code)
-	}
-}
-
-func TestSetCellError(t *testing.T) {
-	tbl, err := New("GEO", []string{"COL"})
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	err = tbl.SetCell("GEO", "WRONG", "a value")
+func TestNew_Error(t *testing.T) {
+	_, err := table.New("")
 	if err == nil {
-		t.Fatalf("expected SetCell to return error")
+		t.Fatal("expected error when New primary is empty")
 	}
 }
 
-func TestSetCell(t *testing.T) {
-	primary := "GEO"
-
-	rows := [][]string{
-		{"HERE", "COLA", "10"},
-		{"HERE", "COLB", "20"},
-		{"THERE", "COLA", "30"},
-		{"THERE", "COLB", "40"},
+func TestGenerate(t *testing.T) {
+	type row struct {
+		geo string
+		cat string
+		val string
 	}
 
-	tbl, err := New(primary, []string{"COLA", "COLB"})
-	if err != nil {
-		t.Fatal(err)
+	var tests = []struct {
+		desc  string
+		input []row
+		want  string
+	}{
+		{
+			desc:  "no input",
+			input: nil,
+			want:  "geography_code\n",
+		},
+		{
+			desc: "single category",
+			input: []row{
+				{"geo", "cat", "val"},
+			},
+			want: "geography_code,cat\ngeo,val\n",
+		},
+		{
+			desc: "multiple categories",
+			input: []row{
+				{"geo", "cat2", "val2"},
+				{"geo", "cat1", "val1"},
+			},
+			want: "geography_code,cat1,cat2\ngeo,val1,val2\n",
+		},
+		{
+			desc: "multiple geographies",
+			input: []row{
+				{"geo2", "cat", "val2"},
+				{"geo1", "cat", "val1"},
+			},
+			want: "geography_code,cat\ngeo1,val1\ngeo2,val2\n",
+		},
+		{
+			desc: "several geographies and categories",
+			input: []row{
+				{"sun", "mass", "1.98847e30"},
+				{"earth", "mass", "5.9722e24"},
+				{"moon", "mass", "7.348e22"},
+				{"sun", "diameter", "1392000"},
+				{"earth", "diameter", "12756"},
+				{"moon", "diameter", "3471"},
+			},
+			want: "geography_code,diameter,mass\nearth,12756,5.9722e24\nmoon,3471,7.348e22\nsun,1392000,1.98847e30\n",
+		},
 	}
 
-	for _, row := range rows {
-		err := tbl.SetCell(row[0], row[1], row[2])
+	for _, test := range tests {
+		tbl, err := table.New("geography_code")
 		if err != nil {
-			t.Fatal(err)
+			t.Errorf("%s: %s", test.desc, err)
+			continue
 		}
-	}
 
-	want := fmt.Sprintf("%s,COLA,COLB\nHERE,10,20\nTHERE,30,40\n", primary)
+		// populate table from test.input
+		for _, r := range test.input {
+			tbl.SetCell(r.geo, r.cat, r.val)
+		}
 
-	buf := strings.Builder{}
-	err = tbl.Generate(&buf)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if buf.String() != want {
-		t.Fatalf("got %q, want %q", buf.String(), want)
+		// generate csv into buf
+		var buf strings.Builder
+		if err := tbl.Generate(&buf); err != nil {
+			t.Fatalf("can't happen: %s", err)
+		}
+
+		if buf.String() != test.want {
+			t.Errorf("%s:\n%s\nwant:\n%s\n", test.desc, buf.String(), test.want)
+		}
 	}
 }
