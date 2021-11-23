5e9485949259af193ae0ff15ef0a029d79a5c7f6
Steve Mynott <steve.mynott@gmail.com>
2021-11-23 09:30:40 +0000

    populate geo with actual geocode names

---
+++ dataingest/geo/README.md
@@ -0,0 +1,9 @@
+# populate geo with ONS geo codes
+
+Uses https://ons.maps.arcgis.com/home/item.html?id=db8e133607244b58a1e2e5dca433391e
+
+(Code History Database (June 2021) for the United Kingdom)
+
+Javascript download to 'Code_History_Database_(June_2021)_UK'
+
+Extract and use ChangeHistory.csv & Equivalents.csv
---
+++ dataingest/geo/main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/ONSdigital/dp-find-insights-poc-api/model"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func readCsvFile(filePath string) (records [][]string) {
+	func() {
+		f, err := os.Open(filePath)
+		if err != nil {
+			log.Fatal("Unable to read input file "+filePath,
+				err)
+		}
+		defer f.Close()
+
+		csvReader := csv.NewReader(f)
+		records, err = csvReader.ReadAll()
+
+		if err != nil {
+			log.Fatal("Unable to parse file as CSV for "+
+				filePath, err)
+		}
+	}()
+
+	return records
+}
+
+func main() {
+
+	dsn := fmt.Sprintf(
+		"postgres://%s:%s@%s:%s/%s",
+		os.Getenv("PGUSER"),
+		os.Getenv("PGPASSWORD"),
+		os.Getenv("PGHOST"),
+		os.Getenv("PGPORT"),
+		os.Getenv("PGDATABASE"),
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Print(err)
+	}
+
+	fmt.Println(dsn)
+
+	parseCVS(db, "Equivalents.csv")
+	parseCVS(db, "ChangeHistory.csv")
+}
+
+func parseCVS(db *gorm.DB, file string) {
+
+	records := readCsvFile(file)
+	m := make(map[string]string)
+
+	for _, r := range records {
+		if r[1] != "" {
+			m[r[0]] = r[1]
+		}
+	}
+
+	var geos []model.Geo
+
+	db.Find(&geos)
+
+	for i := range geos {
+		g := geos[i]
+
+		if m[g.Code] != "" {
+			fmt.Print(g.Code)
+			fmt.Print(" ")
+			fmt.Println(m[g.Code])
+			g.Name = m[g.Code]
+			db.Save(&g)
+		}
+
+	}
+}
