fff933772d770736d0c2cca65c4ce906b16191c3
Steve Mynott <steve.mynott@gmail.com>
2021-11-30 14:58:13 +0000

    use geography column

--- dataingest/spatial/import.sh
+++ dataingest/spatial/import.sh
@@ -15,7 +15,7 @@ tables["lad_gis"]="Local_Authority_Districts_(December_2017)_Boundaries_in_the_U
 for TABLE in "${!tables[@]}"; do
     GEOJSON="${tables[$TABLE]}"
     echo "creating '$TABLE' in '$PGDATABASE' on '$PGHOST'"
-    docker run $EXTRA -v $PWD:$PWD $DOCKER ogr2ogr -f "PostgreSQL" PG:"host=$PGHOST user=$PGUSER dbname=$PGDATABASE password=$PGPASSWORD port=$PGPORT" "$PWD/$GEOJSON" -nln "$TABLE"  --config PG_USE_COPY YES 
+    docker run $EXTRA -v $PWD:$PWD $DOCKER ogr2ogr -f "PostgreSQL" PG:"host=$PGHOST user=$PGUSER dbname=$PGDATABASE password=$PGPASSWORD port=$PGPORT" "$PWD/$GEOJSON" -nln "$TABLE" --config PG_USE_COPY YES -lco GEOM_TYPE=geography
     psql -c "VACUUM ANALYZE $TABLE"
 done
 
