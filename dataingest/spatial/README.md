df86162ac8e99066294ca224865efefccf26d15c
Steve Mynott <steve.mynott@gmail.com>
2021-11-30 14:37:03 +0000

    import

---
+++ dataingest/spatial/README.md
@@ -0,0 +1,16 @@
+* AWS
+https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/Appendix.PostgreSQL.CommonDBATasks.PostGIS.html
+
+Location of GeoJSON download
+
+* LAD
+
+https://geoportal.statistics.gov.uk/datasets/ons::local-authority-districts-december-2017-boundaries-in-the-uk-wgs84/explore?layer=3&location=54.650000%2C-3.250000%2C5.66
+
+Source of "Local_Authority_Districts_(December_2017)_Boundaries_in_the_UK_(WGS84).geojson"
+
+* LSOA
+
+https://geoportal.statistics.gov.uk/datasets/lower-layer-super-output-areas-december-2011-boundaries-super-generalised-clipped-bsc-ew-v3/explore?location=52.371030%2C-2.190838%2C10.90
+
+Source of "Lower_Layer_Super_Output_Areas_(December_2011)_Boundaries_Super_Generalised_Clipped_(BSC)_EW_V3.geojson"
---
+++ dataingest/spatial/import.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+if [[ $1 == linux-localhost ]]; then
+    EXTRA=--network="host"
+fi
+
+DOCKER=osgeo/gdal:alpine-small-3.3.3
+
+declare -A tables
+tables["lsoa_gis"]="Lower_Layer_Super_Output_Areas_(December_2011)_Boundaries_Super_Generalised_Clipped_(BSC)_EW_V3.geojson"
+tables["lad_gis"]="Local_Authority_Districts_(December_2017)_Boundaries_in_the_UK_(WGS84).geojson"
+
+for TABLE in "${!tables[@]}"; do
+    GEOJSON="${tables[$TABLE]}"
+    echo "creating '$TABLE' in '$PGDATABASE' on '$PGHOST'"
+    docker run $EXTRA -v $PWD:$PWD $DOCKER ogr2ogr -f "PostgreSQL" PG:"host=$PGHOST user=$PGUSER dbname=$PGDATABASE password=$PGPASSWORD port=$PGPORT" "$PWD/$GEOJSON" -nln "$TABLE"
+    psql -c "VACUUM ANALYZE $TABLE"
+done
+
+
