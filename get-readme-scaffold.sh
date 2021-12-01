2922f92e6c291d314c9401fad1c3a7d3ee13d56b
Vivian Allen <vivian.allen@methods.co.uk>
2021-12-01 14:53:35 +0000

    update markdown generated by get-readme-scaffold.sh

--- get-readme-scaffold.sh
+++ get-readme-scaffold.sh
@@ -6,13 +6,14 @@ outfile="${thisDir}/readme-scaffold.md"
 # prep outfile
 projName=$(basename "$thisDir")
 printf "# ${projName}\n" > "$outfile"
+printf "\n## Project Contents\n" >> "$outfile"
 
 # loop through all non-hidden folders and try 'go doc --all' in them, write output to outfile if meaningful
 find . -type d -not -path '*/\.*' | while read -r dir
 do
-    printf "\n## dir: ${dir}\n" >> "$outfile"
+    printf "\n### dir: ${dir}\n" >> "$outfile"
     # print autodocs for any go code
-    outContent=$(go doc -all "$dir" 2>/dev/null) && printf "### public Go code:\n\`\`\` ${outContent} \n\`\`\`\n" >> "${outfile}"
+    outContent=$(go doc -all "$dir" 2>/dev/null) && printf "\n*public Go code:*\n\`\`\` ${outContent} \n\`\`\`\n" >> "${outfile}"
     # link any readmes
     cat "${dir}/README.md" &>/dev/null && printf "See [README](${dir}/README.md) for more details.\n" >> "${outfile}"
 
