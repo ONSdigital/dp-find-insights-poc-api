cfea510ac37fcadd72e84a655d62a2699ad8d774
Vivian Allen <vivian.allen@methods.co.uk>
2021-12-01 14:22:44 +0000

    get-readme-scaffold.sh makes readme scaffold from go doc output
    shell script to crawl project looking for public go code. which is
    then documented with `go doc` and appended to readme-scaffold.md
    as preformatted text. Any README.md files found in package
    directories will be linked to in readme-scaffold.md.
    
    This change needed to make it easier to document this project.

---
+++ get-readme-scaffold.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+thisDir=$PWD
+outfile="${thisDir}/readme-scaffold.md"
+
+# prep outfile
+projName=$(basename "$thisDir")
+printf "# ${projName}\n" > "$outfile"
+
+# loop through all non-hidden folders and try 'go doc --all' in them, write output to outfile if meaningful
+find . -type d -not -path '*/\.*' | while read -r dir
+do
+    printf "\n## dir: ${dir}\n" >> "$outfile"
+    # print autodocs for any go code
+    outContent=$(go doc -all "$dir" 2>/dev/null) && printf "### public Go code:\n\`\`\` ${outContent} \n\`\`\`\n" >> "${outfile}"
+    # link any readmes
+    cat "${dir}/README.md" &>/dev/null && printf "[LINK TO README](${dir}/README.md)\n" >> "${outfile}"
+
+done
