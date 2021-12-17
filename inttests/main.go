50ac97e9192d5e9e3828925c007ceb9283d04b38
Vivian Allen <vivian.allen@methods.co.uk>
2021-12-17 11:21:04 +0000

    WIP 1:1 change skinny -> census in inttests

--- inttests/main.go
+++ inttests/main.go
@@ -20,112 +20,105 @@ type APITest = struct {
 	query        string
 }
 
-const baseURL = `https://5laefo1cxd.execute-api.eu-central-1.amazonaws.com/dev/hello/skinny`
-const baseURLLocal = `http://localhost:25252/dev/hello/skinny`
+const baseURL = `https://5laefo1cxd.execute-api.eu-central-1.amazonaws.com/dev/hello/census`
+const baseURLLocal = `http://localhost:25252/dev/hello/census`
 
 var Tests = []APITest{
-	// all rows single col
-	{
-		"all rows single col",
-		baseURL,
-		baseURLLocal,
-		`cols=QS802EW0009`,
-	},
 	// all cols single row
 	{
 		"all cols single row",
 		baseURL,
 		baseURLLocal,
-		`rows=E01002111`,
+		`cols=geography_code&rows=E01002111`,
 	},
 	// single row + col
 	{
 		"rows param single cols param single",
 		baseURL,
 		baseURLLocal,
-		`rows=E01000001&cols=QS119EW0001`,
+		`rows=E01000001&cols=geography_code,QS119EW0001`,
 	},
 	// multi rows
 	{
 		"rows param multi single cols param single",
 		baseURL,
 		baseURLLocal,
-		`rows=E01000001&rows=E01000222&rows=E01000333&cols=QS117EW0001`,
+		`rows=E01000001&rows=E01000222&rows=E01000333&cols=geography_code,QS117EW0001`,
 	},
 	{
 		"rows param multi array cols param single",
 		baseURL,
 		baseURLLocal,
-		`rows=E01000100,E01000110,E01000200&cols=QS119EW0003`,
+		`rows=E01000100,E01000110,E01000200&cols=geography_code,QS119EW0003`,
 	},
 	{
 		"rows param multi range cols param single",
 		baseURL,
 		baseURLLocal,
-		`rows=E01001111...E01001211&cols=QS118EW0011`,
+		`rows=E01001111...E01001211&cols=geography_code,QS118EW0011`,
 	},
 	{
 		"rows param multi mixed cols param single",
 		baseURL,
 		baseURLLocal,
-		`rows=E01000001&rows=E01000100,E01000110,E01000200&rows=E01001111...E01001211&cols=QS118EW0011`,
+		`rows=E01000001&rows=E01000100,E01000110,E01000200&rows=E01001111...E01001211&cols=geography_code,QS118EW0011`,
 	},
 	// multi cols
 	{
 		"rows param single cols param multi single",
 		baseURL,
 		baseURLLocal,
-		`rows=E01000001&cols=QS119EW0001&cols=QS118EW0001&cols=QS117EW0001`,
+		`rows=E01000001&cols=QS119EW0001&cols=geography_code,QS118EW0001&cols=QS117EW0001`,
 	},
 	{
 		"rows param single cols param multi array",
 		baseURL,
 		baseURLLocal,
-		`rows=E01000001&cols=QS119EW0001,QS119EW0002,QS119EW0003`,
+		`rows=E01000001&cols=geography_code,QS119EW0001,QS119EW0002,QS119EW0003`,
 	},
 	{
 		"rows param single cols param multi range",
 		baseURL,
 		baseURLLocal,
-		`rows=E01000001&cols=QS118EW0001...QS118EW0011`,
+		`rows=E01000001&cols=geography_code,QS118EW0001...QS118EW0011`,
 	},
 	{
 		"rows param single cols param multi mixed",
 		baseURL,
 		baseURLLocal,
-		`rows=E01000001&cols=QS117EW0001&cols=QS119EW0001,QS119EW0002,QS119EW0003&cols=QS118EW0001...QS118EW0011`,
+		`rows=E01000001&cols=QS117EW0001&cols=geography_code,QS119EW0001,QS119EW0002,QS119EW0003&cols=QS118EW0001...QS118EW0011`,
 	},
 	// multi rows + cols
 	{
 		"rows param multi single cols param multi single",
 		baseURL,
 		baseURLLocal,
-		`rows=E01000001&rows=E01000222&rows=E01000333&cols=QS119EW0001&cols=QS118EW0001&cols=QS117EW0001`,
+		`rows=E01000001&rows=E01000222&rows=E01000333&cols=geography_code,QS119EW0001&cols=QS118EW0001&cols=QS117EW0001`,
 	},
 	{
 		"rows param multi array cols param multi array",
 		baseURL,
 		baseURLLocal,
-		`rows=E01000100,E01000110,E01000200&cols=QS119EW0001,QS119EW0002,QS119EW0003`,
+		`rows=E01000100,E01000110,E01000200&cols=geography_code,QS119EW0001,QS119EW0002,QS119EW0003`,
 	},
 	{
 		"rows param multi range cols param multi range",
 		baseURL,
 		baseURLLocal,
-		`rows=E01001111...E01001211&cols=QS118EW0001...QS118EW0011`,
+		`rows=E01001111...E01001211&cols=geography_code,QS118EW0001...QS118EW0011`,
 	},
 	{
 		"rows param multi mixed cols param multi mixed",
 		baseURL,
 		baseURLLocal,
-		`rows=E01000001&rows=E01000100,E01000110,E01000200&rows=E01001111...E01001211&cols=QS117EW0001&cols=QS119EW0001,QS119EW0002,QS119EW0003&cols=QS118EW0001...QS118EW0011`,
+		`rows=E01000001&rows=E01000100,E01000110,E01000200&rows=E01001111...E01001211&cols=QS117EW0001&cols=geography_code,QS119EW0001,QS119EW0002,QS119EW0003&cols=QS118EW0001...QS118EW0011`,
 	},
 	// bbox
 	{
 		"bbox param cols param multi mixed",
 		baseURL,
 		baseURLLocal,
-		`bbox=0.1338,51.4635,0.1017,51.4647&cols=QS117EW0001&cols=QS119EW0001,QS119EW0002,QS119EW0003&cols=QS118EW0001...QS118EW0011&geotype=LSOA`,
+		`bbox=0.1338,51.4635,0.1017,51.4647&cols=geography_code,QS117EW0001&cols=QS119EW0001,QS119EW0002,QS119EW0003&cols=QS118EW0001...QS118EW0011&geotype=LSOA`,
 		//`https://5laefo1cxd.execute-api.eu-central-1.amazonaws.com/dev/hello/skinny?bbox=51.4635,0.1338,51.4647,0.1017&cols=QS117EW0001&cols=QS119EW0001,QS119EW0002,QS119EW0003&cols=QS118EW0001...QS118EW0011&geotype=LSOA`,
 	},
 }
