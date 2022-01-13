af111822446a9954d8538d7de29345dc9534f6c7
Daniel Lawrence <33934706+wavemechanics@users.noreply.github.com>
2022-01-13 11:35:59 +0000

    create find-insights-db-dumps bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -377,3 +377,14 @@ resource "aws_secretsmanager_secret" "fi-pg" {
     Project = var.project_tag
   }
 }
+
+# bucket to hold db dumps
+#
+resource "aws_s3_bucket" "db-dumps" {
+  bucket = "find-insights-db-dumps"
+  acl    = "private"
+
+  tags = {
+    Project = var.project_tag
+  }
+}
