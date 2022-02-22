e76bb1e9ac705bc7df09e3312777fb98a502457d
Steve Mynott <steve.mynott@gmail.com>
2022-02-22 09:07:55 +0000

    import

---
+++ cantabular/class-query.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# Eleanor: Usual-Residents and People-Households
+echo "searching for '$1'"
+
+echo "Usual-Residents"
+go run . -ds Usual-Residents -class $1
+#echo "Household-Ref-Persons"
+#go run . -ds Household-Ref-Persons -long $1
+echo "People-Households"
+go run . -ds People-Households -class $1
+
+
---
+++ cantabular/vars-query.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Eleanor: Usual-Residents and People-Households
+
+echo "searching for '$1'"
+
+echo "Usual-Residents"
+go run . -ds Usual-Residents -variables | grep -i $1
+#echo "Household-Ref-Persons"
+#go run . -ds Household-Ref-Persons -short | grep -i $1
+echo "People-Households"
+go run . -ds People-Households -variables | grep -i $1
