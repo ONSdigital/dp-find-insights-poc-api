da4d6a53b850453098d0bb6708ed0e62878b2b1c
Steve Mynott <steve.mynott@gmail.com>
2022-03-03 10:09:33 +0000

    delete unused test and add benchmark type test

--- cantabular/cantabular_test.go
+++ cantabular/cantabular_test.go
@@ -3,6 +3,7 @@ package cantabular
 import (
 	"fmt"
 	"log"
+	"strings"
 	"testing"
 )
 
@@ -75,105 +76,15 @@ func TestSpecificMetricsOtherDS(t *testing.T) { // QUERY 2
 	}
 }
 
-/*
+// query all codes as a crude benchmark
 func TestRespFilterMetrics(t *testing.T) {
-	for code := range shortVarMap() {
+	for code := range ShortVarMap() {
 
-		var query MetricFilter
-		SendQueryVars(&query, BuildCantParam("E92000001", code))
-		got := ParseMetricFilter(&query)
-
-		if !strings.HasPrefix(got, "\"cantabular") {
+		geoq, catsq, values := QueryMetricFilter("", "E92000001", "Country", code)
+		got := ParseMetric(geoq, catsq, values)
+		if !strings.HasPrefix(got, "cantabular") {
 			t.Fatalf(got)
 		}
 
 	}
 }
-
-
-func TestSpecificFilterMetrics(t *testing.T) {
-
-	{
-		code := "QS501EW"
-		geo := []string{"E92000001"}
-
-			if getGeoTypeName(geo) != "Country" {
-				t.Fail()
-
-			}
-
-		var query MetricFilter2
-		SendQueryVars(&query, BuildCantParam(geo, code))
-
-		exp := "\"cantabular\",\"No qualifications\",\"Level 1 qualifications\",\"Level 2 qualifications\",\"Level 3 qualifications\",\"Level 4 qualifications and above\",\"Apprenticeships and other qualifications\",\"Not applicable\"\n\"geography_code\",\"10\",\"11\",\"12\",\"14\",\"15\",\"13,16\",\"-9\"\nE92000001,9539010,5680447,6571675,5306778,12117875,3952125,9710552\n"
-
-		geoq := query.Dataset.Table.Dimensions[0].Categories
-		catsq := query.Dataset.Table.Dimensions[1].Categories
-		values := query.Dataset.Table.Values
-		got := ParseMetric2(geoq, catsq, values)
-
-		if got != exp {
-			fmt.Printf("%#v\n", got)
-			t.Fail()
-		}
-	}
-
-	{
-		code := "QS302EW"
-		geo := "E06000004"
-
-		name := getGeoTypeName(geo)
-		if name != "LA" {
-			t.Errorf("got " + name)
-
-		}
-
-		var query MetricFilter2
-		SendQueryVars(&query, BuildCantParam(geo, code))
-
-		exp := "\"cantabular\",\"Very good or good health\",\"Fair health\",\"Bad or very bad health\",\"Not applicable\"\n\"geography_code\",\"1-2\",\"3\",\"4-5\",\"-9\"\nE06000004,141570,26818,13058,0\n"
-
-		geoq := query.Dataset.Table.Dimensions[0].Categories
-		catsq := query.Dataset.Table.Dimensions[1].Categories
-		values := query.Dataset.Table.Values
-		got := ParseMetric2(geoq, catsq, values)
-		if got != exp {
-			t.Errorf(got)
-		}
-
-	}
-
-	{
-		code := "QS406EW"
-		geo := "E02003802"
-
-		name := getGeoTypeName(geo)
-		if name != "MSOA" {
-			t.Errorf("got " + name)
-		}
-
-		ds := getDataSet("QS406EW")
-		if ds != "People-Households" {
-			t.Errorf("got " + ds)
-		}
-
-		var query MetricFilter2
-		vars := BuildCantParam(geo, code)
-		q.Q(vars)
-		SendQueryVars(&query, vars)
-
-		exp := "\"cantabular\",\"1 person in household\",\"2 people in household\",\"3 people in household\",\"4 people in household\",\"5 people in household\",\"6 or more people in household\",\"Not applicable\"\n\"geography_code\",\"1\",\"2\",\"3\",\"4\",\"5\",\"6-9\",\"-9\"\nE02003802,1049,2398,1845,2004,1015,580,184\n"
-		geoq := query.Dataset.Table.Dimensions[0].Categories
-		catsq := query.Dataset.Table.Dimensions[1].Categories
-		values := query.Dataset.Table.Values
-		got := ParseMetric2(geoq, catsq, values)
-
-		if got != exp {
-			fmt.Printf("%#v\n", got)
-			t.Fail()
-		}
-
-	}
-
-}
-*/
