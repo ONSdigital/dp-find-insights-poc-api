6480385f1bbd692cecbb78c12b6e3087a52d428e
Vivian Allen <vivian.allen@methods.co.uk>
2022-03-23 10:59:11 +0000

    add geotype to geo endpoint respone
    Refactor geo endpoint to include geotype in API response. Also
    dry up geo SQL generation slightly.

--- pkg/geodata/geo.go
+++ pkg/geodata/geo.go
@@ -11,50 +11,51 @@ import (
 
 type Resp struct {
 	Meta struct {
-		Name string `json:"name"`
-		Code string `json:"code"`
+		Name    string `json:"name"`
+		Code    string `json:"code"`
+		Geotype string `json:"geotype"`
 	} `json:"meta"`
 	GeoJSON *geojson.FeatureCollection `json:"geo_json"`
 }
 
 func (app *Geodata) Geo(ctx context.Context, year int, geocode string, geoname string) (*Resp, error) {
-	var template string
+	queryString := `
+	SELECT
+		ST_AsBinary(geo.wkb_long_lat_geom),
+		ST_AsBinary(geo.wkb_geometry),
+		ST_AsBinary(ST_BoundingDiagonal(geo.wkb_geometry)),
+		geo.name,
+		geo.code,
+		geo_type.name
+	FROM
+		geo,
+		geo_type
+	`
+	var conditionString string
 	var queryCondition string
 	if geocode != "" {
-		template = `
-			SELECT
-				ST_AsBinary(wkb_long_lat_geom),
-				ST_AsBinary(wkb_geometry),
-				ST_AsBinary(ST_BoundingDiagonal(wkb_geometry)),
-				name,
-				code
-			FROM geo
-			WHERE code = $1
+		conditionString = `
+			WHERE geo.code = $1
+			AND geo.type_id = geo_type.id
 			`
 		queryCondition = geocode
 	} else {
-		template = `
-			SELECT
-				ST_AsBinary(wkb_long_lat_geom),
-				ST_AsBinary(wkb_geometry),
-				ST_AsBinary(ST_BoundingDiagonal(wkb_geometry)),
-				name,
-				code
-			FROM geo
-			WHERE name = $1
+		conditionString = `
+			WHERE geo.name = $1
+			AND geo.type_id = geo_type.id
 			`
 		queryCondition = geoname
 	}
-
-	stmt, err := app.db.DB().PrepareContext(ctx, template)
+	fullQueryString := queryString + conditionString
+	stmt, err := app.db.DB().PrepareContext(ctx, fullQueryString)
 	if err != nil {
 		return nil, err
 	}
 	defer stmt.Close()
 
 	var centroid, boundary, bbox []byte
-	var name, code string
-	err = stmt.QueryRowContext(ctx, queryCondition).Scan(&centroid, &boundary, &bbox, &name, &code)
+	var name, code, geotype string
+	err = stmt.QueryRowContext(ctx, queryCondition).Scan(&centroid, &boundary, &bbox, &name, &code, &geotype)
 	if err != nil {
 		if err == sql.ErrNoRows {
 			return nil, sentinel.ErrNoContent
@@ -86,6 +87,7 @@ func (app *Geodata) Geo(ctx context.Context, year int, geocode string, geoname s
 	r := &Resp{GeoJSON: collection}
 	r.Meta.Name = name
 	r.Meta.Code = code
+	r.Meta.Geotype = geotype
 
 	return r, nil
 }
